e53c5b1f33f96a7dbd1d9e17cbfb888b004f8623
Omkar Sase <saseomkar214@gmail.com>
2023-11-12 22:42:40 +0530

    day 11

--- 30 days python + sql challenge/day 10/classesAndObjects.py
+++ 30 days python + sql challenge/day 10/classesAndObjects.py
@@ -11,14 +11,35 @@
 # print(student1.name)
 
 class Student:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    college = 'pce'  # class variable
 
-  def __str__(self):
-    return f"student created with name {self.name} and age {self.age}"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"student created with name {self.name} and age {self.age}"
+
+    @classmethod
+    def getCollegeName(cls):
+        return cls.college
+
+    @staticmethod
+    def printCollegeName():
+        print()
+
+    def __del__(self):
+        print("Destructor called")
+
 
 s1 = Student("John", 36)
 
 print(s1.name, s1.age)
 print(s1)
+
+print(Student.college)
+
+print("start")
+Student.printCollegeName()
+print(Student.getCollegeName())
+print("end")
