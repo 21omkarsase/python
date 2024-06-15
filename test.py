2e810d4cfff90de00e693de805943cb649a5e4e1
omkar sase <saseomkar214@gmail.com>
2024-06-16 02:12:15 +0530

    deleted unwanted files

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ class Test:
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
-        pass
         
     def getDetails(self):
         return {"name":self.__name, "age": self.__age}
